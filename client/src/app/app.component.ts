0ef3c163bd023bc3a83b9900e01857e3bb4e9db4
Dino <elvedingran@gmail.com>
2022-03-23 15:36:51 +0100

    feat: add client login and register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { User } from './_models/user';
+import loader from "@angular-devkit/build-angular/src/webpack/plugins/single-test-transform";
+import {AccountService} from "./_services/account.service";
 
 @Component({
   selector: 'app-root',
@@ -10,18 +13,16 @@ export class AppComponent implements OnInit{
   title = 'client';
   tracks: any;
 
-  constructor(private http: HttpClient) {
+  constructor(private accountService: AccountService) {
   }
 
   ngOnInit() {
-      this.getTracks();
+      this.setCurrentUser();
     }
 
-    getTracks(){
-      this.http.get('https://localhost:5001/api/track').subscribe(x => {
-        this.tracks=x;
-      }, error => {
-        console.log(error);
-      })
+    setCurrentUser(){
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
     }
+
 }
