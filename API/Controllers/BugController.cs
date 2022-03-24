2692d6888633ef1dc4bcb0198c78b46921fb4258
Dino <elvedingran@gmail.com>
2022-03-24 19:27:48 +0100

    Add exception handling

---
+++ API/Controllers/BugController.cs
@@ -0,0 +1,54 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class BugController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public BugController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "test text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var testing = _context.Users.Find(-1);
+
+            if (testing == null) return NotFound();
+
+            return Ok(testing);
+        }
+
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var testing = _context.Users.Find(-1);
+
+            var returnTesting = testing.ToString();
+
+            return returnTesting;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<AppUser> GetBadRequest()
+        {
+            return BadRequest("Request was not good");
+        }
+    }
+}
---
+++ API/Entities/ExeptionHandlingData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class ExeptionHandlingData
+    {
+        public int Id { get; set; }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
+}
